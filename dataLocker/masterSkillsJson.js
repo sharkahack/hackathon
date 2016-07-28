9d7bfcf89414d43ea3dde7559e8fb762f22608d4
Michael Boston <mboston30@gmail.com>
2016-07-28 10:14:08 -0400

    adding the data json files

---
+++ dataLocker/masterSkillsJson.js
@@ -0,0 +1,11 @@
+(function(){
+  "use strict"
+  dataLocker.masterSkills = [
+    {id: 1, name: "Javascript"},
+    {id: 2, name: "HTML"},
+    {id: 3, name: "PhotoShop"},
+    {id: 4, name: "CSS"},
+    {id: 5, name: "Less"},
+    {id: 6, name: "TypeScript"},
+  ]
+})();
