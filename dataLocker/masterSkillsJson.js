270488d8e2e21535c87a485117be1caceb2a6231
nbarbier <nicolabarbieri@gmail.com>
2016-07-29 10:37:03 -0400

    new master

--- dataLocker/masterSkillsJson.js
+++ dataLocker/masterSkillsJson.js
@@ -2,12 +2,12 @@
   "use strict"
   angular.module('skillsMatrixApp').run(function($rootScope){
     $rootScope.dataLocker.masterSkills = [
-      {id: 1, name: "Javascript"},
-      {id: 2, name: "HTML"},
-      {id: 3, name: "PhotoShop"},
-      {id: 4, name: "CSS"},
-      {id: 5, name: "Less"},
-      {id: 6, name: "TypeScript"},
+      "Javascript",
+      "HTML",
+      "PhotoShop",
+      "CSS",
+      "Less",
+      "TypeScript",
     ]
   });
 
