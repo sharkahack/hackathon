e090f11a775b3f110b8c0dee5b90a9a23ef0346b
Ho <kho@na.global.ad>
2016-07-29 12:08:16 -0400

    keyword library

---
+++ dataLocker/keywordLibrary.js
@@ -0,0 +1,217 @@
+(function(){
+  "use strict"
+  angular.module('skillsMatrixApp').run(function($rootScope) {
+      $rootScope.dataLocker.keywordLibrary = [
+          {
+            "text": "Financial Analyst Begin"
+          },
+          {
+            "text": "forecasting"
+          },
+          {
+            "text": "Budgeting"
+          },
+          {
+            "text": "reporting"
+          },
+          {
+            "text": "Financial Analyst"
+          },
+          {
+            "text": "Hyperion"
+          },
+          {
+            "text": "essbase"
+          },
+          {
+            "text": "Accounting"
+          },
+          {
+            "text": "Audting"
+          },
+          {
+            "text": "Payroll"
+          },
+          {
+            "text": "Operating Expense"
+          },
+          {
+            "text": "General Ledger"
+          },
+          {
+            "text": "Booking"
+          },
+          {
+            "text": "Support process"
+          },
+          {
+            "text": "SL margin"
+          },
+          {
+            "text": "accounting standards"
+          },
+          {
+            "text": "accounting principles"
+          },
+          {
+            "text": "budgeting process"
+          },
+          {
+            "text": "operational metrics"
+          },
+          {
+            "text": "functional P&L"
+          },
+          {
+            "text": "business decisions"
+          },
+          {
+            "text": "Operational P&L"
+          },
+          {
+            "text": "standardized reporting"
+          },
+          {
+            "text": "Corporate FP&A"
+          },
+          {
+            "text": "problem-solving abilities"
+          },
+          {
+            "text": "Microsoft Excel"
+          },
+          {
+            "text": "ad-hoc analysis"
+          },
+          {
+            "text": "Financial Analyst End"
+          },
+          {
+            "text": "Web Developer Begin"
+          },
+          {
+            "text": "Web Developer"
+          },
+          {
+            "text": "Senior Web Developer"
+          },
+          {
+            "text": "Web Application Developer"
+          },
+          {
+            "text": "Programmer"
+          },
+          {
+            "text": "Software Engineer"
+          },
+          {
+            "text": "Senior Software Engineer"
+          },
+          {
+            "text": "Software Application Developer"
+          },
+          {
+            "text": "Full Stack"
+          },
+          {
+            "text": "HTML5"
+          },
+          {
+            "text": "Javascript"
+          },
+          {
+            "text": "JQuery"
+          },
+          {
+            "text": "Ajax"
+          },
+          {
+            "text": "LINQ"
+          },
+          {
+            "text": "CSS3"
+          },
+          {
+            "text": "Angular"
+          },
+          {
+            "text": "Bootstrap"
+          },
+          {
+            "text": "Responsive"
+          },
+          {
+            "text": "MVC"
+          },
+          {
+            "text": "IoC"
+          },
+          {
+            "text": ".NET"
+          },
+          {
+            "text": "C#"
+          },
+          {
+            "text": "VB.NET"
+          },
+          {
+            "text": "SQL"
+          },
+          {
+            "text": "SQL Server"
+          },
+          {
+            "text": "Web Services"
+          },
+          {
+            "text": "Oracle"
+          },
+          {
+            "text": "Java"
+          },
+          {
+            "text": "J2EE"
+          },
+          {
+            "text": "Framework"
+          },
+          {
+            "text": "Python"
+          },
+          {
+            "text": "Ruby"
+          },
+          {
+            "text": "Ruby on Rails"
+          },
+          {
+            "text": "Apache"
+          },
+          {
+            "text": "Tomcat"
+          },
+          {
+            "text": "Rest"
+          },
+          {
+            "text": "Matlab"
+          },
+          {
+            "text": "SOLR"
+          },
+          {
+            "text": "Lucene"
+          },
+          {
+            "text": "Cloud Computing"
+          },
+          {
+            "text": "Web Developer End"
+          },
+          {
+            "text": "Keyword Library End"
+          }
+      ]
+    });
+})();
